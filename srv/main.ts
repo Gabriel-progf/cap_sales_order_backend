068151e09f3ce6654ea190fd9038dacb60d8b663
Gabriel Oliveira de Macedo <gabriel.ol.macedo@hotmail.com>
2025-09-08 20:26:23 -0300

    feat: baixa estoque ao criar ordem de venda

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,5 @@
 import cds, { Request, Service } from "@sap/cds";
-import { Customers, Products, SalesOrderItem, SalesOrderItems } from "@models/sales";
+import { Customers, Product, Products, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from "@models/sales";
 
 export default (service: Service) => {
   service.after("READ", "Customers", (result: Customers) => {
@@ -12,8 +12,11 @@ export default (service: Service) => {
   });
 
   service.before("CREATE", "SalesOrderHeaders", async (request: Request) => {
+
     const params = request.data;
-    const items: SalesOrderItems =  params.items
+
+    const items: SalesOrderItems =  params.items;
+
     if (!params.customer_id) {
       return request.reject(400, "Invalid Customer");
     }
@@ -22,35 +25,54 @@ export default (service: Service) => {
       return request.reject(400, "Invalid Item");
     }
 
-    const customerQuery = SELECT.one
-      .from("sales.Customers")
-      .where({ id: params.customer_id });
+    const customerQuery = SELECT.one.from("sales.Customers").where({ id: params.customer_id });
+
     const customer = await cds.run(customerQuery);
 
     if (!customer) {
       return request.reject(404, "Customer not found");
     }
 
-    const productsIds: string[] = params.items.map(
-      (item: SalesOrderItem) => item.product_id
-    );
-    const productSQuery = SELECT.from("sales.Products").where({
-      id: productsIds,
-    });
+    const productsIds: string[] = params.items.map((item: SalesOrderItem) => item.product_id);
+
+    const productSQuery = SELECT.from("sales.Products").where({id: productsIds});
+
     const products: Products = await cds.run(productSQuery);
+
     for (const item of items) {
-      const dbProduct = products.find(
-        (product) => product.id == item.product_id
-      );
+
+      const dbProduct = products.find((product) => product.id == item.product_id);
+
       if (!dbProduct) {
         return request.reject(404, `Produto ${item.product_id} não encontrado`);
       }
+
       if (dbProduct.stock === 0) {
-        return request.reject(
-          400,
-          `Produto ${dbProduct.name} (${dbProduct.id}) sem estoque disponível`
-        );
+        return request.reject(400, `Produto ${dbProduct.name} (${dbProduct.id}) sem estoque disponível`);
       }
+      
     }
+
   });
+
+  service.after('CREATE', 'SalesOrderHeaders', async (results) => {
+      const headersAsArray = Array.isArray(results) ? results : [results] as SalesOrderHeaders;
+      for (const header of headersAsArray) {
+          const items = header.items as SalesOrderItems;
+          const productsData = items.map(item => ({
+              id: item.product_id as string,
+              quantity: item.quantity as number
+          }));
+          const productsIds: string[] = productsData.map((productData) => productData.id);
+          const productsQuery = SELECT.from('sales.Products').where({ id: productsIds });
+          const products: Products = await cds.run(productsQuery);
+          for (const productData of productsData) {
+              const foundProduct = products.find(product => product.id === productData.id) as Product;
+              foundProduct.stock = (foundProduct.stock as number) - productData.quantity;
+              await cds.update('sales.Products').where({ id: foundProduct.id }).with({ stock: foundProduct.stock });
+          }
+      }
+  });
+
+
 };
